5ea632c656671fba184350e3bec78d87527df77b
Mateusz Ollek <mateuszollek@gmail.com>
2023-01-15 23:21:29 +0100

    Simple tests

--- test.py
+++ test.py
@@ -7,63 +7,63 @@ if __name__ == "__main__":
     
     db = Database()
 
-    # rows = db.select_all_tickets()
-    # for row in rows:
-    #     print(row)
-
-    # rows = db.select_all_users()
-    # for row in rows:
-    #     print(row)
-
-
-    # user = db.select_user('john.doe@email.com')
-    # print('Select user: ', user)
-
-    # rows = db.select_current_performances()
-    # for row in rows:
-    #     print(row)
-
     p_uuid = uuid.uuid1()
-    db.insert_performance(dt.datetime.strptime('2023-01-16', '%Y-%m-%d'), dt.datetime.strptime('2023-01-16 12:00', '%Y-%m-%d %H:%M'), 'Little Red Riding Hood'.lower(), dt.datetime.strptime('2023-01-16 13:15', '%Y-%m-%d %H:%M'), p_uuid)
+    p_date = dt.datetime.strptime('2023-01-16', '%Y-%m-%d')
+    start_date = dt.datetime.strptime('2023-01-16 12:00','%Y-%m-%d %H:%M')
+    title = 'Little Red Riding Hood'.lower()
+    end_date = dt.datetime.strptime('2023-01-16 13:15', '%Y-%m-%d %H:%M')
+    db.insert_performance(p_date,
+                            start_date,
+                            title,
+                            end_date,
+                            p_uuid)
+    r = db.session.execute(db.session.prepare("SELECT COUNT(*) as cnt from performances where p_date=? and start_date=? and title=?;"), [p_date, start_date,title])
+    if r.one().cnt == 1:
+        print("INSERT into performances success")
+    else:
+        print("INSERT into performances fail")
+
+    
     db.insert_performance_seats_batch(p_uuid, [x for x in range(1,51)], ['Little Red Riding Hood'.lower()],[dt.datetime.strptime('2023-01-16 12:00', '%Y-%m-%d %H:%M')],[None])
 
-    db.update_performance_seat_take_seat_batch(p_uuid, [4,5,6], 'john.doe@email.com')
+    r = db.session.execute(db.session.prepare("SELECT COUNT(*) as cnt from performance_seats where performance_id=?;"),[p_uuid])
+    if r.one().cnt == 50:
+        print("INSERT into performance_seats success")
+    else:
+        print("INSERT into performance_seats fail")
 
-#    db.update_performance_seat_take_seat(p_uuid, 4, 'cezary@theatre.com')
 
-#    db.update_performance_seat_take_seat_batch(p_uuid, [1,2,3,4], 'john.doe@email.com')
+    db.update_performance_seat_take_seat_batch(p_uuid, [4,5,6], 'john@email.com')
+    r = db.session.execute(db.session.prepare("SELECT taken_by from performance_seats where performance_id=? and seat_number in ?;"),[p_uuid, [4,5,6]])
+    broke = False
+    for ri in r:
+        if ri.taken_by != 'john@email.com':
+            broke = True
+            print('UPDATE performance seat failed')
+            break
+    if not broke:
+        print('UPDATE performance seat success')
 
-    #db.update_performance_seat_take_seat_batch(p_uuid, [7,8,9,10], 'john.doe@email.com')
+    db.update_performance_seat_take_seat_batch(p_uuid, [4,5,6], 'kate@email.com')
+    r = db.session.execute(db.session.prepare("SELECT taken_by from performance_seats where performance_id=? and seat_number in ?;"),[p_uuid, [4,5,6]])
+    broke = False
+    for ri in r:
+        if ri.taken_by == 'kate@email.com':
+            broke = True
+            print('Failed UPDATE performance seat failed')
+            break
+    if not broke:
+        print('Failed UPDATE performance seat success')
 
-    result = db.insert_user_ticket_batch('john.doe@email.com', p_uuid, [4,5,6], ['john','joe', 'dezz'], ['doe', 'mamma', 'nuts'])
-    print(result)
+    db.insert_user_ticket_batch('john@email.com', p_uuid, [4,5,6], ['john','joe', 'dezz'], ['doe', 'mamma', 'nuts'])
+    r = db.session.execute(db.session.prepare("SELECT count(*) as cnt from tickets where email='john@email.com' and performance_id=?;"),[p_uuid])
+    if r.one().cnt == 3:
+        print("INSERT into tickets success")
+    else:
+        print("INSERT into tickets fail")
     #db.insert_user_ticket('john.doe@email.com', p_uuid, 4, 'john', 'doe')
 
 
     db.finalize()
-'''
-    p_uuid2 = uuid.uuid1()
-    db.insert_performance( dt.datetime.strptime('2023-01-17', '%Y-%m-%d'), dt.datetime.strptime('2023-01-17 13:00', '%Y-%m-%d %H:%M'), 'Shrek'.lower(), dt.datetime.strptime('2023-01-17 15:00', '%Y-%m-%d %H:%M'), p_uuid2)
-    db.insert_performance_seats_batch(p_uuid2, [x for x in range(1,51)], ['Shrek'.lower()],[dt.datetime.strptime('2023-01-17 15:00', '%Y-%m-%d %H:%M')],[None])
 
-    p_uuid3 = uuid.uuid1()
-    db.insert_performance(dt.datetime.strptime('2023-01-18', '%Y-%m-%d'), dt.datetime.strptime('2023-01-18 13:00', '%Y-%m-%d %H:%M'), 'Shrek'.lower(), dt.datetime.strptime('2023-01-18 15:00', '%Y-%m-%d %H:%M'), p_uuid3)
-    db.insert_performance_seats_batch(p_uuid3, [x for x in range(1,51)], ['Shrek'.lower()],[dt.datetime.strptime('2023-01-18 15:00', '%Y-%m-%d %H:%M')],[None])
-
-
-
-    # rows = db.select_all_performances()
-    # for row in rows:
-    #     print(row)
-
-    # rows = db.select_all_performance_seats()
-    # for row in rows:
-    #     print(row)
-
-    rows = db.select_performances_by_dates([dt.datetime.strptime('2023-01-17', '%Y-%m-%d'),
-                                            dt.datetime.strptime('2023-01-18', '%Y-%m-%d')])
-
-    for row in rows:
-        print(row)
-'''
 
